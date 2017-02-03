4b3e44ee4a39cfe98aee4fb910616f6f810fa2b0
hpaasch <hepaasch@gmail.com>
2017-02-03 12:28:49 -0500

    Sticky footer implemented. Homepage 3 items.

--- nabes_app/views.py
+++ nabes_app/views.py
@@ -44,6 +44,13 @@ class IndexView(ListView):
     template_name = 'index.html'
     model = PublicPost
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['top_post_list'] = PublicPost.objects.filter(priority='Top')[:1]
+        context['middle_post_list'] = PublicPost.objects.filter(priority='Middle')[:1]
+        context['bottom_post_list'] = PublicPost.objects.filter(priority='Bottom')[:1]
+        return context
+
 
 class CreateAccountView(CreateView):
     model = User
