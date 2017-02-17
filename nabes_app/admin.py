c0216d0c83795bb932ce0dd9ba9b9525c7631a40
hpaasch <hepaasch@gmail.com>
2017-02-17 08:15:51 -0500

    added username to staff directory view. added family names to admin.

--- nabes_app/admin.py
+++ nabes_app/admin.py
@@ -8,9 +8,14 @@ class OfficerAdmin(admin.ModelAdmin):
     search_fields = ['name', 'title', 'term', 'phone', 'email']
 
 
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['primary_last_name', 'family_member_names']
+    search_fields = ['primary_last_name', 'family_member_names']
+
+
 admin.site.register(PublicPost)
 admin.site.register(MemberPost)
 admin.site.register(BoardPost)
 admin.site.register(Newsletter)
-admin.site.register(Profile)
+admin.site.register(Profile, ProfileAdmin)
 admin.site.register(Officer, OfficerAdmin)
